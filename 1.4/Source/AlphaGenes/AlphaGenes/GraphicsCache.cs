1ce5347cd91ba66ed4e4892a8f40c1df45804829
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-09-25 16:42:25 +0200

    Initial commit

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/GraphicsCache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+
+namespace AlphaGenes
+{
+    [StaticConstructorOnStartup]
+    public static class GraphicsCache
+    {
+       
+   
+        //public static readonly Texture2D Paste = ContentFinder<Texture2D>.Get("UI/Buttons/Paste", true);
+
+
+
+
+
+
+    }
+}
---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Harmony/HarmonyPatchers.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using System.Collections.Generic;
+using RimWorld.Planet;
+using System.Linq;
+using System;
+using RimWorld.BaseGen;
+
+
+
+namespace AlphaGenes
+{
+
+    
+
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.alphagenes");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+
+
+   
+
+
+
+}
