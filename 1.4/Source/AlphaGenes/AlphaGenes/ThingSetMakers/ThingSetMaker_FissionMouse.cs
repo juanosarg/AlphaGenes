420da699568c6bbafc7a5f779d6fab96a492cbe2
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-15 15:31:58 +0200

    v1.0

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/ThingSetMakers/ThingSetMaker_FissionMouse.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace AlphaGenes
+{
+
+    public class ThingSetMaker_FissionMouse : ThingSetMaker
+    {
+        protected override void Generate(ThingSetMakerParams parms, List<Thing> outThings)
+        {
+
+            PawnKindDef mouse = PawnKindDef.Named("AA_FissionMouse");
+
+            PawnGenerationRequest request = new PawnGenerationRequest(mouse, Faction.OfPlayer, PawnGenerationContext.NonPlayer);
+            Pawn pawn = PawnGenerator.GeneratePawn(request);
+            Pawn pawn2 = PawnGenerator.GeneratePawn(request);
+            Pawn pawn3 = PawnGenerator.GeneratePawn(request);
+            outThings.Add(pawn);
+            outThings.Add(pawn2);
+            outThings.Add(pawn3);
+
+
+
+        }
+
+        protected override IEnumerable<ThingDef> AllGeneratableThingsDebugSub(ThingSetMakerParams parms)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
+
---
+++ 1.4/Source/AlphaGenes/AlphaGenes/ThingSetMakers/ThingSetMaker_OcularJelly.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace AlphaGenes
+{
+
+    public class ThingSetMaker_OcularJelly : ThingSetMaker
+    {
+        protected override void Generate(ThingSetMakerParams parms, List<Thing> outThings)
+        {
+
+            PawnKindDef jelly = PawnKindDef.Named("AA_OcularJelly");
+
+            PawnGenerationRequest request = new PawnGenerationRequest(jelly, Faction.OfPlayer, PawnGenerationContext.NonPlayer);
+            Pawn pawn = PawnGenerator.GeneratePawn(request);
+         
+            outThings.Add(pawn);
+          
+
+
+        }
+
+        protected override IEnumerable<ThingDef> AllGeneratableThingsDebugSub(ThingSetMakerParams parms)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
+
