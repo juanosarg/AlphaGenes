bb39fa5d14ffd5fb5d7c018a99e533f86cad2fbb
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-18 16:53:51 +0200

    v1.0

--- 1.4/Source/AlphaGenes/AlphaGenes/HediffComps/HediffComp_RandomMutation.cs
+++ 1.4/Source/AlphaGenes/AlphaGenes/HediffComps/HediffComp_RandomMutation.cs
@@ -4,37 +4,89 @@ using System.Linq;
 using RimWorld;
 using Verse;
 
+
 namespace AlphaGenes
 {
 	public class HediffComp_RandomMutation : HediffComp
 	{
 		private HediffCompProperties_RandomMutation Props => (HediffCompProperties_RandomMutation)props;
 
-		public GeneDef gene;
+		public List<GeneDef> genes = new List<GeneDef>();
+
+		public bool Active = false;
 
         public override void CompExposeData()
         {
             base.CompExposeData();
-			Scribe_Defs.Look(ref this.gene, nameof(this.gene));
+			Scribe_Collections.Look(ref this.genes, nameof(this.genes));
+			Scribe_Values.Look(ref this.Active, nameof(this.Active));
+
 		}
 
-        public override void CompPostPostAdd(DamageInfo? dinfo)
-		{
-			base.CompPostPostAdd(dinfo);
+		public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (!Active && this.parent.pawn.Map!=null) {
+				Active = true;
+				genes.Clear();
+				List<string> geneNamesToDisplay = new List<string>();
+				for (int i = 0; i < Props.numberOfGenes; i++)
+				{
+					GeneDef gene = DefDatabase<GeneDef>.AllDefs.Where((GeneDef x) => x.exclusionTags?.Contains("AG_OnlyOnCharacterCreation") == false).RandomElement();
+					genes.Add(gene);
+					geneNamesToDisplay.Add(gene.LabelCap);
+					this.parent.pawn.genes?.AddGene(gene, true);
+
+				}
+                Messages.Message(    "AG_RandomGenesGained".Translate(parent.pawn.LabelShortCap, geneNamesToDisplay.ToCommaList()), this.parent.pawn, MessageTypeDefOf.PositiveEvent, true);
+
+			}
+
+            if (Props.recurrent)
+            {
+                if (this.parent.pawn.IsHashIntervalTick(Props.period)) {
+
+                    if (!genes.NullOrEmpty()) {
+						for (int i = 0; i < Props.numberOfGenes; i++)
+						{
+							if (this.parent.pawn.genes?.GetGene(genes[i]) != null)
+							{
+								this.parent.pawn.genes?.RemoveGene(this.parent.pawn.genes.GetGene(genes[i]));
+							}
+
+						}
+						genes.Clear();
+					}
+					
+
+					Active = false;
+
+				}
+            }
 			
-			gene = DefDatabase<GeneDef>.AllDefs.Where((GeneDef x) => x.exclusionTags?.Contains("AG_OnlyOnCharacterCreation") == false).RandomElement();
-			this.parent.pawn.genes?.AddGene(gene,true);
-			Messages.Message("AG_RandomGeneGained".Translate(gene.LabelCap), this.parent.pawn, MessageTypeDefOf.PositiveEvent, true);
+
 		}
+		
 
         public override void CompPostPostRemoved()
         {
             base.CompPostPostRemoved();
-            if (this.parent.pawn.genes?.GetGene(gene) != null)
-            {
-				this.parent.pawn.genes?.RemoveGene(this.parent.pawn.genes.GetGene(gene));
+
+			for (int i = 0; i < Props.numberOfGenes; i++)
+			{
+				if (this.parent.pawn.genes?.GetGene(genes[i]) != null)
+				{
+					this.parent.pawn.genes?.RemoveGene(this.parent.pawn.genes.GetGene(genes[i]));
+				}
+
 			}
-			
+			Active = false;
+			genes.Clear();
+
+
+
+
 		}
 
 
--- 1.4/Source/AlphaGenes/AlphaGenes/HediffComps/Properties/HediffCompProperties_RandomMutation.cs
+++ 1.4/Source/AlphaGenes/AlphaGenes/HediffComps/Properties/HediffCompProperties_RandomMutation.cs
@@ -4,7 +4,9 @@ namespace AlphaGenes
 {
 	public class HediffCompProperties_RandomMutation : HediffCompProperties
 	{
-		
+		public int numberOfGenes = 1;
+		public bool recurrent = false;
+		public int period = 60000;
 
 		public HediffCompProperties_RandomMutation()
 		{
