4bd89be18b85213dd11b94549c26934c29d9cc7c
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-23 13:59:33 +0200

    v1.0

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Comps/CompProperties/CompProperties_TargetEffect_Endogenefier.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AlphaGenes
+{
+	public class CompProperties_TargetEffect_Endogenefier : CompProperties
+	{
+		
+
+		public CompProperties_TargetEffect_Endogenefier()
+		{
+			compClass = typeof(CompTargetEffect_Endogenefier);
+		}
+	}
+}
---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Comps/CompProperties/CompProperties_TargetEffect_Xenogenefier.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AlphaGenes
+{
+	public class CompProperties_TargetEffect_Xenogenefier : CompProperties
+	{
+		
+
+		public CompProperties_TargetEffect_Xenogenefier()
+		{
+			compClass = typeof(CompTargetEffect_Xenogenefier);
+		}
+	}
+}
