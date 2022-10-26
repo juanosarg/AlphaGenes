9cac6a7e76ad04c80482edc04ec2db80f87388cc
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-26 13:06:20 +0200

    Added mineral shock

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Abilities/Properties/CompProperties_AbilityMineralCost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AlphaGenes
+{
+
+
+ 
+
+    public class CompProperties_AbilityMineralCost : CompProperties_AbilityEffect
+    {
+        public float mineralCost;
+
+        public CompProperties_AbilityMineralCost()
+        {
+            compClass = typeof(CompAbilityEffect_MineralCost);
+        }
+
+        public override IEnumerable<string> ExtraStatSummary()
+        {
+            yield return (string)("AG_AbilityMineralCost".Translate() + ": ") + Mathf.RoundToInt(mineralCost * 100f);
+        }
+    }
+}
