4666ba6d599b65ba28c2286137e5e71d661ebf51
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-10-28 08:55:25 +0200

    New gen toolkits

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Comps/CompProperties/CompProperties_TargetEffect_XenotypeNullifier.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+
+namespace AlphaGenes
+{
+	public class CompProperties_TargetEffect_XenotypeNullifier : CompProperties
+	{
+
+
+		public CompProperties_TargetEffect_XenotypeNullifier()
+		{
+			compClass = typeof(CompTargetEffect_XenotypeNullifier);
+		}
+	}
+}
---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Comps/CompTargetEffect_GermlineMutator.cs
@@ -0,0 +1,70 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.Sound;
+
+
+namespace AlphaGenes
+{
+    public class CompTargetEffect_GermlineMutator : CompTargetEffect
+    {
+
+
+
+
+        public CompProperties_TargetEffect_GermlineMutator Props
+        {
+            get
+            {
+                return (CompProperties_TargetEffect_GermlineMutator)this.props;
+            }
+        }
+
+
+
+
+
+        public override void DoEffectOn(Pawn user, Thing target)
+        {
+
+
+            if (user.IsColonistPlayerControlled && user.CanReserveAndReach(target, PathEndMode.Touch, Danger.Deadly))
+            {
+                Pawn pawn = target as Pawn;
+
+
+                Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(InternalDefOf.AG_GeneRemovalComa);
+
+                if (hediff == null)
+                {
+                    GeneDef geneToCreate = DefDatabase<GeneDef>.AllDefs.Where((GeneDef x) => x.exclusionTags?.Contains("AG_OnlyOnCharacterCreation") == false).RandomElement();
+                    pawn.genes.AddGene(geneToCreate,false);
+                   
+                    pawn.health.AddHediff(InternalDefOf.AG_GeneRemovalComa);
+                    user.carryTracker.DestroyCarriedThing();
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+}
