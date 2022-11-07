7419ffecc60684c803ce59b72117da9d6d564382
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-11-07 17:09:47 +0100

    Blood type genes

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Gas and Liquid/Persistant_Fuel.cs
@@ -0,0 +1,48 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AlphaGenes
+{
+
+	public class Persistant_Fuel : Filth
+	{
+		private int spawnTick;
+
+		public int dissapearsIn;
+
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look(ref spawnTick, "spawnTick", 0);
+			Scribe_Values.Look(ref dissapearsIn, "dissapearsIn", 0);
+		}
+
+		public override void SpawnSetup(Map map, bool respawningAfterLoad)
+		{
+			base.SpawnSetup(map, respawningAfterLoad);
+			spawnTick = Find.TickManager.TicksGame;
+			dissapearsIn = (int)this.def.filth.disappearsInDays.RandomInRange * GenDate.TicksPerDay;
+		}
+
+		public void Refill()
+		{
+			spawnTick = Find.TickManager.TicksGame;
+		}
+
+		public override void Tick()
+		{
+			if (spawnTick + dissapearsIn < Find.TickManager.TicksGame)
+			{
+				Destroy();
+			}
+		}
+
+		public override void ThickenFilth()
+		{
+			base.ThickenFilth();
+			Refill();
+		}
+	}
+
+
+}
