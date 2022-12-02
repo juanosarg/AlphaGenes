a5b368ec646959ea86b9d484a4684d68e59a1230
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-12-02 12:57:58 +0100

    v1.2

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Abilities/CompDetonate.cs
@@ -0,0 +1,49 @@
+﻿
+using Verse;
+using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+
+
+
+
+namespace AlphaGenes
+{
+    class CompDetonate : CompAbilityEffect
+    {
+
+        new public CompProperties_Detonate Props
+        {
+            get
+            {
+                return (CompProperties_Detonate)this.props;
+            }
+        }
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            Detonate();
+        }
+
+        public void Detonate()
+        {
+            List<Thing> ignoredThings = new List<Thing>();
+            if (!Props.damageUser)
+            {
+                ignoredThings.Add(this.parent.pawn);
+            }
+
+            GenExplosion.DoExplosion(parent.pawn.Position, parent.pawn.Map, Props.radius, Props.damageType, parent.pawn, Props.damageAmount, Props.damagePenetration, Props.soundCreated, null, null, null,
+                null, 0f, 1, GasType.BlindSmoke, false, null, 0f, 1, Props.chanceToStartFire, false, null, ignoredThings);
+
+
+
+        }
+
+
+
+
+    }
+}
