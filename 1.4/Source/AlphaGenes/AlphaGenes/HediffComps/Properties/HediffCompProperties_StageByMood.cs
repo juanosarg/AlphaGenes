e306e1e008b7429cd85b496ce7fd8c16fe3359c4
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-12-16 15:08:54 +0100

    v1.3

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/HediffComps/Properties/HediffCompProperties_StageByMood.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaGenes.HediffComps.Properties
+{
+    internal class HediffCompProperties_StageByMood
+    {
+    }
+}
