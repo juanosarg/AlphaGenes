e14a43c242fa4a2a2b8d242985b26e7e287b8518
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-12-16 17:56:00 +0100

    v1.3

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Abilities/CompInsanityBlast.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+
+namespace AlphaGenes
+{
+    public class CompInsanityBlast : CompAbilityEffect
+    {
+       
+        private new CompProperties_InsanityBlast Props => (CompProperties_InsanityBlast)props;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            
+            base.Apply(target, dest);
+            Pawn pawn = target.Pawn;
+
+            if (!pawn.Dead)
+            {
+                System.Random rand = new System.Random();
+                float psychicSensitivity = pawn.GetStatValue(StatDefOf.PsychicSensitivity);
+                bool applyShock = false;
+                if (psychicSensitivity > 1)
+                {
+                    applyShock = true;
+
+                }
+                else
+                {
+                    if(rand.NextDouble()< psychicSensitivity)
+                    {
+                        applyShock = true;
+                    }
+                    else
+                    {
+                        Messages.Message("AG_ResistedInsanityBlast".Translate(pawn), pawn, MessageTypeDefOf.RejectInput, historical: false);
+                    }
+
+
+                }
+
+                if (applyShock)
+                {
+                    pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.Berserk, null, forceWake: true);
+
+                }
+                
+
+            }
+
+        }
+
+       
+
+
+
+      
+
+
+
+
+    }
+}
