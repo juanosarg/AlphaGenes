f27828795467626963d7ac112ef16ad4b9f8647d
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-09-23 16:39:35 +0200

    v1.8

---
+++ 1.4/Source/AlphaGenes/AlphaGenes/Abilities/Properties/CompProperties_TailGrapple.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AlphaGenes
+{
+
+
+    public class CompProperties_TailGrapple : CompProperties_AbilityEffect
+    {
+
+     
+
+        public CompProperties_TailGrapple()
+        {
+            this.compClass = typeof(CompTailGrapple);
+        }
+    }
+}
--- 1.4/Source/AlphaGenes/AlphaGenes/StaticCollections/StaticCollectionsClass.cs
+++ 1.4/Source/AlphaGenes/AlphaGenes/StaticCollections/StaticCollectionsClass.cs
@@ -16,8 +16,19 @@ namespace AlphaGenes
         // Number of colonists with the Ocular Affinity gene currently on the world 
         public static int ocular_gene_colonists_inWorld;
 
+        public static Dictionary<Pawn, int> colonist_and_random_mood = new Dictionary<Pawn, int>();
 
-       
+        public static void AddColonistAndRandomMood(Pawn pawn, int mood)
+        {
+            if (pawn != null)
+            {
+
+                colonist_and_random_mood[pawn] = mood;
+            }
+
+
+        }
+
 
 
     }
