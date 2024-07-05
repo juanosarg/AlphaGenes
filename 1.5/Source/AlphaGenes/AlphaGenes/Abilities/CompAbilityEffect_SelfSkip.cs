680118f763fc47e772433bd0702d70c6014f9519
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-05 19:52:43 +0200

    v3.1

---
+++ 1.5/Source/AlphaGenes/AlphaGenes/Abilities/CompAbilityEffect_SelfSkip.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+namespace AlphaGenes
+{
+    public class CompAbilityEffect_SelfSkip : CompAbilityEffect_Teleport
+    {
+
+        public new CompProperties_AbilityTeleport Props => (CompProperties_AbilityTeleport)props;
+
+        public override string ExtraLabelMouseAttachment(LocalTargetInfo target)
+        {
+            return CanSkipTarget(target).Reason;
+        }
+
+        private AcceptanceReport CanSkipTarget(LocalTargetInfo target)
+        {
+            Pawn pawn;
+            if ((pawn = target.Thing as Pawn) != null)
+            {
+               
+                
+                if (parent.pawn != pawn)
+                {
+                    return "AG_NeedToTargetCaster".Translate();
+                }
+            }
+            return true;
+        }
+
+        public override bool Valid(LocalTargetInfo target, bool showMessages = true)
+        {
+            Pawn pawn;
+            if ((pawn = target.Thing as Pawn) != null)
+            {
+
+
+                if (parent.pawn != pawn)
+                {
+                    Messages.Message("AG_NeedToTargetCaster".Translate(), pawn, MessageTypeDefOf.RejectInput, historical: false);
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+
+        }
+}
