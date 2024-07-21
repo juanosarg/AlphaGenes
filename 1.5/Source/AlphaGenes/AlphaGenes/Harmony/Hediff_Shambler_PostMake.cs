8659a6cd4ee5acd12e82884d073cd0203304e995
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-07-21 08:22:43 +0200

    v3.103

--- 1.5/Source/AlphaGenes/AlphaGenes/Harmony/Hediff_Shambler_PostMake.cs
+++ 1.5/Source/AlphaGenes/AlphaGenes/Harmony/Hediff_Shambler_PostMake.cs
@@ -22,27 +22,33 @@ namespace AlphaGenes
 
             if (AlphaGenes_Mod.settings.AG_RandomGenesToShamblers && __instance.pawn.genes!=null)
             {
-                List<GeneDef> genes = DefDatabase<GeneDef>.AllDefsListForReading.Where(x => x.geneClass== typeof(Gene_Shambler)).ToList();
-                if (__instance.pawn.genes.HasActiveGene(InternalDefOf.AG_Shambler_Plagued))
+                if(__instance.pawn.genes.GenesListForReading.Where(x => x.def.geneClass == typeof(Gene_Shambler) && x.def != InternalDefOf.AG_Shambler_Plagued).Count() <= 0)
                 {
-                    genes.Remove(InternalDefOf.AG_Shambler_Plagued);
-                }
-                IntRange numberToRemoveGasProducers = new IntRange(1, 10);
-                if (numberToRemoveGasProducers.RandomInRange != 5)
-                {
-                    genes.Remove(InternalDefOf.AG_Shambler_Toxic);
-                    genes.Remove(InternalDefOf.AG_Shambler_Stinker);
-                    genes.Remove(InternalDefOf.AG_Shambler_DeadlifeBelcher);
-                    genes.Remove(InternalDefOf.AG_Shambler_Polluter);
+                    List<GeneDef> genes = DefDatabase<GeneDef>.AllDefsListForReading.Where(x => x.geneClass == typeof(Gene_Shambler) && x != InternalDefOf.AG_Shambler_DeadlifeBelcher).ToList();
+                    if (__instance.pawn.genes.HasActiveGene(InternalDefOf.AG_Shambler_Plagued))
+                    {
+                        genes.Remove(InternalDefOf.AG_Shambler_Plagued);
+                    }
+                    IntRange numberToRemoveGasProducers = new IntRange(1, 10);
+                    if (numberToRemoveGasProducers.RandomInRange != 5)
+                    {
+                        genes.Remove(InternalDefOf.AG_Shambler_Toxic);
+                        genes.Remove(InternalDefOf.AG_Shambler_Stinker);
+                        genes.Remove(InternalDefOf.AG_Shambler_Polluter);
+                    }
+
+
+                    IntRange numberToAdd = new IntRange(1, 3);
+                    List<GeneDef> genesToAdd = genes.InRandomOrder().Take(numberToAdd.RandomInRange).ToList();
+                    foreach (GeneDef geneToAdd in genesToAdd)
+                    {
+                        __instance.pawn.genes.AddGene(geneToAdd, true);
+
+                    }
                 }
 
-               
-                IntRange numberToAdd = new IntRange(1, 3);
-                List<GeneDef> genesToAdd = genes.InRandomOrder().Take(numberToAdd.RandomInRange).ToList(); 
-                foreach (GeneDef geneToAdd in genesToAdd) {
-                    __instance.pawn.genes.AddGene(geneToAdd, true);
+
                 
-                }
             }
 
 
