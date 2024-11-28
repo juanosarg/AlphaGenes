3c56139e2709ae2a7841a30c2d13d479676dee87
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-11-28 16:43:18 +0100

    v3.202

---
+++ 1.5/Source/AlphaGenes/AlphaGenes/Harmony/JobGiver_OptimizeApparel_ApparelScoreGain.cs
@@ -0,0 +1,28 @@
+﻿
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+
+
+namespace AlphaGenes
+{
+    [HarmonyPatch(typeof(JobGiver_OptimizeApparel), "ApparelScoreGain")]
+    internal class AlphaGenes_JobGiver_OptimizeApparel_ApparelScoreGain_Postfix
+    {
+
+
+        [HarmonyPostfix]
+        private static void PostFix(ref float  __result, Pawn pawn, Apparel ap)
+        {
+            if (AlphaGenes_EquipmentUtility_CanEquip_Postfix.blockedWeapons.Contains(ap.def) && !pawn.HasActiveGene(InternalDefOf.AG_ForsakenKnowledge))
+            {
+                __result = -1000;
+               
+            }
+
+
+        }
+    }
+}
---
+++ 1.5/Source/AlphaGenes/AlphaGenes/Harmony/Test.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using RimWorld;
+using RimWorld.QuestGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+using Verse.AI;
+using static HarmonyLib.Code;
+
+namespace AlphaGenes
+{
+  /*  [HarmonyPatch(typeof(Building_VoidMonolith), "CanActivate")]
+    public static class AlphaGenesBuilding_VoidMonolith_CanActivate_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var codes = codeInstructions.ToList();
+            var check = AccessTools.Method(typeof(AlphaGenesBuilding_VoidMonolith_CanActivate_Patch), "GetNumber");
+            var fieldLoaded = AccessTools.Field(typeof(MonolithLevelDef), "entityCountCompletionRequired");
+
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+
+                if (codes[i].opcode == OpCodes.Ldfld && codes[i].OperandIs(fieldLoaded))
+                {
+
+                    yield return new CodeInstruction(OpCodes.Call, check);
+                   
+                }else yield return codes[i];
+            }
+        }
+
+
+        public static int GetNumber(MonolithLevelDef level)
+        {
+            return 100;
+        }
+
+
+
+
+
+    }
+
+    [HarmonyPatch(typeof(QuestPart_MonolithPart), "UpdateDescription")]
+    public static class AlphaGenesBuilding_QuestPart_MonolithPart_UpdateDescription_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var codes = codeInstructions.ToList();
+            var check = AccessTools.Method(typeof(AlphaGenesBuilding_VoidMonolith_CanActivate_Patch), "GetNumber");
+            var fieldLoaded = AccessTools.Field(typeof(MonolithLevelDef), "entityCountCompletionRequired");
+
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+
+                if (codes[i].opcode == OpCodes.Ldfld && codes[i].OperandIs(fieldLoaded))
+                {
+
+                    yield return new CodeInstruction(OpCodes.Call, check);
+
+                }
+                else yield return codes[i];
+            }
+        }
+
+
+    
+
+
+
+
+    }
+  */
+}
