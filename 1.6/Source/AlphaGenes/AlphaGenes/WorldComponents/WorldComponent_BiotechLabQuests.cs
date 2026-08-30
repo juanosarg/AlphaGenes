51cb46e73a54bed4955335fd6f17c6f17aec70f3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-08-30 15:22:02 +0200

    v4.104

--- 1.6/Source/AlphaGenes/AlphaGenes/WorldComponents/WorldComponent_BiotechLabQuests.cs
+++ 1.6/Source/AlphaGenes/AlphaGenes/WorldComponents/WorldComponent_BiotechLabQuests.cs
@@ -2,18 +2,20 @@
 using RimWorld.Planet;
 using Verse;
 using RimWorld.QuestGen;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlphaGenes
 {
-
-
 
     public class WorldComponent_BiotechLabQuests : WorldComponent
     {
         public int tickCounter;
         public int ticksToNextQuest = 60000 * 14;
 
-
+        public int forsakenTickCounter = forsakenTickInterval;
+        public const int forsakenTickInterval = 10000;
+     
         public WorldComponent_BiotechLabQuests(World world) : base(world)
         {
         }
@@ -21,9 +23,6 @@ namespace AlphaGenes
         public override void FinalizeInit(bool fromLoad)
         {
             base.FinalizeInit(fromLoad);
-
-
-
         }
 
         public override void WorldComponentTick()
@@ -43,18 +42,37 @@ namespace AlphaGenes
                     ticksToNextQuest = (int)(60000 * Rand.RangeInclusive(15, 30) * AlphaGenes_Mod.settings.AG_QuestRate);
                     tickCounter = 0;
 
-
-
-
                 }
                 tickCounter++;
             }
 
-            
+            if (forsakenTickCounter > forsakenTickInterval)
+            {
+                List<Pawn> allPawns = PawnsFinder.AllMapsCaravansAndTravellingTransporters_Alive_Colonists.InRandomOrder().ToList();
+                bool forgeAccepted=false;
+                foreach (Pawn pawn in allPawns)
+                {
+                    if (pawn.genes?.HasActiveGene(InternalDefOf.AG_ForsakenKnowledge) == true)
+                    {
+                        if (VEF.Buildings.StaticCollectionsClass.hidden_designators.Contains(InternalDefOf.AG_ForsakenForge))
+                        {
+                            VEF.Buildings.StaticCollectionsClass.hidden_designators.Remove(InternalDefOf.AG_ForsakenForge);
+                        }
+                        forgeAccepted = true;
+                        break;
+                    }
+                }
+                if (!forgeAccepted) {
+                    if (!VEF.Buildings.StaticCollectionsClass.hidden_designators.Contains(InternalDefOf.AG_ForsakenForge))
+                    {
+                        VEF.Buildings.StaticCollectionsClass.hidden_designators.Add(InternalDefOf.AG_ForsakenForge);
+                    }
+                }
 
+                forsakenTickCounter = 0;
 
-
-
+            }
+            forsakenTickCounter++;
 
         }
 
